aed64aef66f84fd396dad0af6faf2a1ec8c66077
saurabhec59 <saurabhyadav2690@gmail.com>
2026-08-05 21:25:23 +0530

    update body-parser.js to add parsing xml and multipart type contents as well, also read #8 first

--- src/middleware/body-parser.js
+++ src/middleware/body-parser.js
@@ -10,6 +10,8 @@
 
     Query parameters should be parsed separately using URL parsing utilities and should not be handled inside body-parser middleware.
 */
+import { XMLParser } from "fast-xml-parser";
+const xmlParser = new XMLParser(); // we do not want to create new instance of XMLParser for each request so we are creating it once and reusing it for all requests.
 function parseBody1(req){  // #1....
     var body = "";
     req.on("data", function(chunk){
@@ -38,6 +40,7 @@ function parseBody(req){
 
         var data = "";
         var chunkSize = 0;
+        const encodingType = findCharacterEncoding(req); // we do not want to call this function for each chunk received inside req.on("data") because for one request encoding type will be same for all chunks.
         req.on("data", function(chunk){
 
             // although we are already checking content-length header size above but still client can fake that header and send large payload so we are checking the actual size of data received here.
@@ -48,7 +51,24 @@ function parseBody(req){
                 return;
             }
 
+            // #8........
+            if(encodingType){// if encodingType is undefined means client did not send "charset" parameter and fall back to default Node.js encoding
+                if(Buffer.isEncoding(encodingType)){
+                    // Buffer.isEncoding() returns true if the given encoding is supported by Node.js. Also Buffer.isEncoding() is case-insensitive so it will return true for "utf-8" and "UTF-8" both.
+                    data += chunk.toString(encodingType);
+                }
+                else{
+                    // if encodingType is not supported by Node.js then we can not process the request body and we should reject the request.
+                    onFailure(new Error("Unsupported character encoding: " + encodingType));
+                    req.destroy();
+                    return;
+                }
+            }else{
+                data += chunk.toString(); // default encoding is utf-8
+            }
+            /*
             data += chunk;
+            */
         })
 
         req.on("end", function(){
@@ -69,12 +89,56 @@ function parseBody(req){
                 onSuccess(parsedData);
                 return;
             }
+            // #6...... NOTE: I am aware that this is not a complete and fully correct implementation of multipart/form-data parsing, I tried understanding basics of it.
+            else if(contentType && contentType.startsWith("multipart/form-data")){
+                var parsedData = {};
+                // extracting the boundary parameter, clients adds 2 extra hyphens in body that's why we are adding as well.
+                var boundary  = "--" + req.headers["content-type"].split("boundary=")[1];
+                var fields = data.split(boundary); // #7......
+                for(var i = 1; i<fields.length; i++){
+                    var field = fields[i].split("\r\n\r\n");
+                    // trying to get name parameter
+                    var fieldName = field[0].split("name=")[1];// i suspect it will be "name" instead of name;
+                    // trying to get the actual data
+                    var rawData = field[1].trim(); // to remove the \r\n included
+                    parsedData[fieldName] = rawData;
+                }
+                onSuccess(parsedData);
+                return;
+            }
+            else if(contentType && contentType.startsWith("application/xml")){
+                // node.js do not have built-in xml parser so we are using 3rd party library "fast-xml-parser" to parse xml data.
+                try{
+                    var parsedData = xmlParser.parse(data);
+                    onSuccess(parsedData);
+                    return;
+                }catch(e){
+                    onFailure(e);
+                }
+            }
             else
             {
                 onSuccess(data);
             }
         })
     })
+}
+
+// addition this function as utility
+function findCharacterEncoding(req){
+    var contentType = req.headers["content-type"];
+    var encodingType;
+
+    if(contentType){
+        var parameters = contentType.split(";");
+        for(var i = 1; i<parameters.length; i++){
+            if(parameters[i].trim().startsWith("charset")){
+                encodingType = parameters[i].split("=")[1].trim();
+            }
+        }
+    }
+
+    return encodingType;
 }
 
 export { parseBody };
@@ -159,4 +223,67 @@ Actually here we are checking 2 things:
 First before storing a single byte of payload we are checking if client has sent "content-length" and if yes then is it within the limit of MAX_BODY_SIZE. If not then immediately stop further processing.
 Second, sometimes client can fake the "content-length" header and send a large payload so inside the req.on("data") we are calculating actual data received and the moment it exceeds the MAX_BODY_SIZE then we are stopping further processing.
 
+#6.....
+lets understand multipart/form-data format 1st.
+The header looks like : Content-Type: multipart/form-data; boundary=ABC123
+Notice there is one extra parameter called "boundary". But why? Imagine there is a form sending 'name', 'age' and a text file. So the request body will contain all 3 data. Http knows nothing about formate of data, for it
+everything is byte and it sends that byte over the wire as it is. But now server needs to know that in the request payload/body where data of 'name' ends, where data of 'age' starts and ends....
+so server needs to know how to separate the data of each field. That is why client sends a boundary parameter in the header which is a unique string and also client puts that unique string in the request body in between the data of each field.
+So that server can separate using same parameter/boundary as well.
+Lets say we have a form which sends data including 2 fields 'name' and 'age' and a text file. Then the request body containing that payload will look like:
+
+--ABC123
+Content-Disposition: form-data; name="name"
+
+John
+--ABC123
+Content-Disposition: form-data; name="age"
+
+25
+--ABC123--
+
+===> Now notice boundary value is "ABC123" but while inserting in request body, client is adding 2 extra hyphens "--" before boundary value and also after the last field it is adding 2 extra hyphens "--" after boundary value.
+So server should also take care of that while parsing.
+==> Also notice the request body is different then url encoded or json formates where usually only data is sent in request body but here some metadata headers are also sent.
+That's why parsing multipart/form-data is more complex than other formats. Usually pre-built libraries are used. But we are trying a basic implementation of it to get some feel.
+
+#7..... BASED ON ABOVE BODY EXAMPLE,
+after var fields = data.split(boundary);
+fields[0] ==> ""  (empty string)
+fields[1] ==> "\r\nContent-Disposition: form-data; name="name"\r\n\r\nJohn\r\n"   ========> "\r\n" means new line.
+fields[2] ==> "\r\nContent-Disposition: form-data; name="age"\r\n\r\n25\r\n"
+fields[3] ==> "--"  (because of the last boundary with 2 extra hyphens)
+
+now inside the for loop after this line: var field = fields[i].split("\r\n\r\n");
+field[0] ==> "Content-Disposition: form-data; name="name""
+field[1] ==> "John\r\n"  (notice the new line at the end)
+
+#8........
+Weather it a text like: " this is me"
+or html : <h2>"this is html"</h2>
+or json : {
+    "name": "Rahul", "age": 30
+}
+or xml
+
+But these are just collection of characters like alphabets, numbers and special characters like '<', '{', '}', space, next line '\n'.
+Everything is just characters and special characters.
+Each character and special character(@, #, %, space, tab, newline...) have an equivalent numeric value representation like 'A' -> 65..
+But but but...
+Which character will be converted into what numerical value depends upon the encoding type.
+There are many encoding types like ASCII, UTF-8, UTF-16... and they are nothing just defining numerical representation of characters.
+so when computer processes them then it convert each character into ints numerical representation BASED ON ENCODING TYPE USED.
+Then each numerical number gets converted into it's binary representation.as binary(like 01010101 10101110).
+Here it is bits(0's and 1's) but collectively 8 bits called bytes.
+Then these bytes(binary) gets stored in disk or storage or transmitted over network.
+So when we say http did not care about payload/body format then it means it actually did not. Everything transmitting over network in a http protocol inside the req or res body is just bytes(binary).
+It's responsibility of application/server to convert these received bytes into characters using a character encoding and into correct formates.
+
+Now coming at our server side:
+When we say 'chunk' inside the req.on("data") is a buffer means 'chunk' is having raw bytes like 01010101 11001100....
+Node.js uses default encoding UTF-8 to decode those bytes into characters and then these characters gets concatenated to string variable 'data' as shown : data += chunk;
+But when client sends the character encoding along with the payload then server should decode those raw bytes using that encoding.
+Usually clients sends like:
+Content-Type: application/json; charset = utf-8
+Content-Type: application/xml; charset = utf-16
 */
